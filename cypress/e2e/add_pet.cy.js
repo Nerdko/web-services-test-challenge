95a48f824b7202c5548847136767b3cfc56afe42
Nerdko <150620164+Nerdko@users.noreply.github.com>
2023-12-01 14:49:55 -0600

    created pet creation test

--- cypress/e2e/add_pet.cy.js
+++ cypress/e2e/add_pet.cy.js
@@ -1,3 +1,48 @@
 describe("Add pet", () => {
-  it("add a new pet", () => {});
+  const dog = Cypress.env("dog");
+  const petId = Cypress.env("pet_id");
+  const status = Cypress.env("status");
+  it("add a new pet", () => {
+    cy.request({
+      method: "POST",
+      url: Cypress.env("add_pet_uri"),
+      headers: {
+        accept: "application/json",
+        "Content-type": "application/json",
+      },
+      body: {
+        id: 0,
+        category: {
+          id: dog.category.id,
+          name: dog.category.name,
+        },
+        name: dog.name,
+        photoUrls: [],
+        tags: [
+          {
+            id: dog.tag.id,
+            name: dog.tag.name,
+          },
+        ],
+        status: status,
+      },
+    }).then((response) => {
+      expect(response.body).to.deep.eq({
+        id: petId,
+        category: {
+          id: dog.category.id,
+          name: dog.category.name,
+        },
+        name: dog.name,
+        photoUrls: [],
+        tags: [
+          {
+            id: dog.tag.id,
+            name: dog.tag.name,
+          },
+        ],
+        status: status,
+      });
+    });
+  });
 });
