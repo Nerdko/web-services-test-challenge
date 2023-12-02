c19aa5497f0a4109b794ac20beb8c34db6af6982
Nerdko <150620164+Nerdko@users.noreply.github.com>
2023-12-02 13:02:54 -0600

    removed cucumber due to issues in the installation

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("cypress");
-const { preprocessor } = require("@badeball/cypress-cucumber-preprocessor");
-const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+//const { preprocessor } = require("@badeball/cypress-cucumber-preprocessor");
+//const { browserify } = require("@badeball/cypress-cucumber-preprocessor/browserify");
 module.exports = defineConfig({
   reporter: "mochawesome",
   reporterOptions: {
@@ -8,12 +8,11 @@ module.exports = defineConfig({
   },
   e2e: {
     baseUrl: "https://petstore.swagger.io/v2",
-    specPattern: "**/*.feature",
-    setupNodeEvents(on, config) {
-      // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-      preprocessor.addCucumberPreprocessorPlugin(on, config);
-      on("file:preprocessor", browserify.default(config));
-      return config;
-    },
+    //specPattern: "**/*.feature",
+    //setupNodeEvents(on, config) {
+    //  preprocessor.addCucumberPreprocessorPlugin(on, config);
+    // on("file:preprocessor", browserify.default(config));
+    // return config;
+    // },
   },
 });
